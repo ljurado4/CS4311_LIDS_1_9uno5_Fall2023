669c0e27225322b75866b7e63655e99fd073f1b7
Benjamin H <benjaminhansen836@gmail.com>
2023-09-12 15:37:37 -0600

    user input handling in ConfigureCLI menu navigation

--- CLI/config_menu.py
+++ CLI/config_menu.py
@@ -4,4 +4,8 @@ from menu import Menu
 class ConfigureCLI:
     
     def configure(path_config_file: str) -> None:
-        pass
+        menu = Menu()
+        next_menu = input(">> ")
+        #parse xml file and update
+        
+        menu.navigate_next_menu(next_menu)
