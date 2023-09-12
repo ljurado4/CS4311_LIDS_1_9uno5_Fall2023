394dd9dbbb4d2cadba668faf8932e02ebfadd224
Benjamin H <benjaminhansen836@gmail.com>
2023-09-12 12:59:19 -0600

    Added laucher file and main menu class

---
+++ CLI/cli_launcher.py
@@ -0,0 +1,9 @@
+import main_menu as menu
+
+def main():
+    start_menu = menu()
+    menu.show_menu()
+    
+
+if __name__ == "__main__":
+    main()
---
+++ CLI/main_menu.py
@@ -0,0 +1,5 @@
+
+class MainMenu:
+    
+    def show_menu():
+        print("Main Menu")
