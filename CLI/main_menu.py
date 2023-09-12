4468d4804dff9218fa4cc3f6848cff74c6ede660
Benjamin H <benjaminhansen836@gmail.com>
2023-09-12 14:26:48 -0600

    Updated documentation for show_menu function

--- CLI/main_menu.py
+++ CLI/main_menu.py
@@ -1,11 +1,31 @@
+from menu import menu 
 
 class MainMenu:
+    """
+    Represents the main menu of the CLI
+    
+    This class provides a function to display the main menu and navigate
+    to diffrent menu's based on user input.
+    """
 
     def show_menu():
-        print(
-            "name:\n"
-            "IP:\n"
-            "MAC:\n"
-            "PORT:\n"
-        )
+        """Displays the main menu options to the user.
         
+        Presents the main menu promt. After taking input, it 
+        prints details and then uses 'menu' module to navigate to
+        the next appropiate menu
+        """
+
+        menu = menu.Menu()
+
+        print("Main Menu")
+        next_menu = input(">>")
+
+        print(
+            "name: John Doe\n"
+            "IP: 192.168.1.100\n"
+            "MAC: 00:1A:2B:3C:4D:5E\n"
+            "PORT: 8080"
+        )
+        menu.navigate_next_menu(next_menu)
+
