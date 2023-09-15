35ebc31a235e5059b88c76d88a7ae01527e3b470
savelasco28 <savelasco@miners.utep.edu>
2023-09-15 14:45:20 -0600

    Slight menu adjustments

--- CLI/help_menu.py
+++ CLI/help_menu.py
@@ -13,9 +13,11 @@ class HelpMenu:
         """
         
         print("The following are system commands to change from the system windows and what each function does.")
+        print("\tStart Menu: Used to return to the start menu that was displayed upon start up.")
         print("\tConfig: Parse the XML file and updates whitelist/blacklist, etc.")
         print("\tShow PCAP: Display information from the latest PCAP.")
         print("\tAlert: Displays the Alerts.")
+        print("\tExit: Exits the system.")
         menu_helper = menu.Menu()
         next_menu = menu_helper.get_user_input(">> ",menu_helper.choice_set)
         menu.Menu().navigate_next_menu(next_menu)
