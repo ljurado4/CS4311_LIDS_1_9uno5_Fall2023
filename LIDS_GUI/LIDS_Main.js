8c59e5c366e1fa2085001004d67a145cab677703
ArtOlm <89996531+ArtOlm@users.noreply.github.com>
2023-09-21 13:30:15 -0600

    ingested xml file

--- LIDS_GUI/LIDS_Main.js
+++ LIDS_GUI/LIDS_Main.js
@@ -1,6 +1,34 @@
 const fileInput = document.getElementById('myFile');
-fileInput.onchange = () => {
+fileInput.onchange = async () => {
+
+  //get uploaded file
   const selectedFile = fileInput.files[0];
-  sessionStorage.setItem("XMLFILE",selectedFile)
-  window.location = "LIDS_Dashboard.html"
+  let reader = new FileReader()
+  reader.readAsText(selectedFile)
+  //get everything from the file
+  let loadedFile = ""
+  //wait for the file to load
+  await new Promise(resolve => reader.onload = () => resolve());
+  loadedFile = reader.result
+  let parser, xmlDoc;
+
+  //xml parser
+  parser = new DOMParser();
+  xmlDoc = parser.parseFromString(loadedFile,"text/xml");
+
+  let users = new Object()
+ 
+  for(let i = 0;i < xmlDoc.getElementsByTagName("system").length;i++){
+    let currUser = new Object()
+    currUser["name"] = xmlDoc.getElementsByTagName("system")[i].getElementsByTagName("name")[0].childNodes[0].nodeValue
+    currUser["ip"] = xmlDoc.getElementsByTagName("system")[i].getElementsByTagName("ip")[0].childNodes[0].nodeValue
+    currUser["mac"] = xmlDoc.getElementsByTagName("system")[i].getElementsByTagName("mac")[0].childNodes[0].nodeValue
+    currUser["ports"] = xmlDoc.getElementsByTagName("system")[i].getElementsByTagName("ports")[0].childNodes[0].nodeValue
+    currUser["whitelist"] = xmlDoc.getElementsByTagName("system")[i].getElementsByTagName("whitelist")[0].childNodes[0].nodeValue
+    users[i.toString()] = currUser 
+    //needs work - tranferring data to the backend, change any code necessary
+    //console.log(users)
+    //comment line below to not change screen and stay in corruent page to print data
+    window.location = "LIDS_Dashboard.html"
+  }
 }
