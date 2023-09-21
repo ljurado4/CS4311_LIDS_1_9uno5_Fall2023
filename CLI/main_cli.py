2524099edf4b6c4fe747f4e4bd864efebfe6014f
Benjamin H <benjaminhansen836@gmail.com>
2023-09-20 19:44:32 -0600

    Direct user to Alert menu when starting cli

--- CLI/main_cli.py
+++ CLI/main_cli.py
@@ -4,10 +4,9 @@ import menu
 import argparse
 
 def main():
+    alert_menu = menu.Menu()
+    alert_menu.navigate_next_menu("Alert")
 
-    menu_helper = menu.Menu()
-    next_menu = menu_helper.get_user_input(">> ",menu_helper.choice_set)
-    menu_helper.navigate_next_menu(next_menu)
 
 if __name__ == "__main__":
     
