eaf798d98513589326a322707bd05891af3ae2f1
Benjamin H <benjaminhansen836@gmail.com>
2023-09-22 19:03:21 -0600

    Specify custom template and static folders on Flask app

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 
 # Command  .\env\Scripts\activate.bat
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='LIDS_GUI/templates',static_folder='LIDS_GUI/static')
 
 @app.route('/')
 def index():
     return render_template('LIDS_Main.html')
 
-
+@app.route('/LIDS_Dashboard')
+def dashboard():
+    return render_template('LIDS_Dashboard.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
