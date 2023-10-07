93164e8f1fe2c546ec72ecda78e5fc8d92f6ce61
ArtOlm <89996531+ArtOlm@users.noreply.github.com>
2023-10-07 00:14:13 -0600

    major gui update

---
+++ LIDS-D_GUI/LIDS-D.js
@@ -0,0 +1,12 @@
+//counter used to keep track of malicious packets
+let maliciousPacketCount = 0
+
+//adding initial html to malicious packets found
+document.getElementById("mpd").innerHTML = "Malicious Packet Count:\n" + maliciousPacketCount.toString()
+
+//handles the start of th server
+function startServer(){
+    //needs to be implemented
+    window.location = "LIDS-D_Server_Details.html"
+  }
+  
