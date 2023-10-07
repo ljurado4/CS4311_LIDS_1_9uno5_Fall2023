48b5c048c9e0dbc631786dda243ad57f3f98b510
Benjamin H <benjaminhansen836@gmail.com>
2023-10-06 20:13:11 -0600

    Reorganized directory structure for LIDS-D: Moved HTML to 'templates' and moved CSS/JavaScript to 'static' folder

---
+++ LIDS_D/static/js/LIDS-D_RTB.js
@@ -0,0 +1,31 @@
+//file handles functionality on buttons on the right tab bar
+// forms for all buttons on the right tab bar
+const MPFFORM = document.querySelector("#MaliciousPacketCount")
+const CSFROM = document.querySelector("#ConfigServer")
+const VAFROM = document.querySelector("#ViewAlerts")
+const NIFORM = document.querySelector("#NetworkInfo")
+const NMFROM = document.querySelector("#NetworkMapButton")
+
+const CBELMENT = document.getElementById("contentBlock")
+
+MPFFORM.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    CBELMENT.innerHTML = "Malicious Packet Count"
+})
+//missing implementation for previously added files
+CSFROM.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    window.location = "LIDS-D_Config_Server_View.html"
+})
+VAFROM.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    window.location = "LIDS-D_Alerts_View.html"
+})
+NIFORM.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    window.location = "LIDS-D_Network_Info_View.html"
+})
+NMFROM.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    CBELMENT.innerHTML = "Network Map"
+})
