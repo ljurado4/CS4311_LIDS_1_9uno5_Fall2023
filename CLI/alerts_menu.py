e684e70a19202816f772caeec7a4eeab73c45028
Benjamin H <benjaminhansen836@gmail.com>
2023-11-01 20:00:35 -0600

    merge conflits

--- CLI/alerts_menu.py
+++ CLI/alerts_menu.py
@@ -1,3 +1,9 @@
+#alerts_menu.py
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import menu
 ################################################################################
 # File: alerts_menu.py
 #
@@ -16,14 +22,45 @@
 
 # Install 'tabulate' using `pip3 install tabulate`
 from tabulate import tabulate
+import threading
+from backend import packets,alerts_manager
+
 
 class Alerts_CLI:
 
-    def display_Alerts(self, alertList):
-        header = ["LVL", "Time", "IP", "Port", "Description"]
-        data = alertList
-        table = tabulate(data, header, tablefmt="fancy_grid")
-        print(table)
-        menu_helper = menu.Menu()  # Make sure to import the 'menu' module if it's used here.
-        next_menu = menu_helper.get_user_input(">> ", menu_helper.choice_set)
+
+    def display_Alerts(self,alertList):
+        
+        print("Alerts")
+        
+        
+        # Todo 1
+        # Consider moving this after configurations.
+        # To access alerts post-sniffer run on a thread, use this.
+        # Ensure this runs throughout the CLI program, even if the user navigates other menus.
+        # Find a way to always run a thread post-file execution or explore alternative methods.
+        
+        pack_time = packets.PackTime()
+        thread = threading.Thread(target=pack_time.run_sniffer)
+        thread.start()
+        
+     
+        # get alerts 
+        getter = alerts_manager.AlertManager().sharedAlerts
+        
+        
+        # Display alerts
+        # Todo 2
+        # Goal: Continuously display alerts in this menu without user intervention.
+        # Problems:
+        # 1. How can we accept input without disrupting the alert display?
+        # 2. How do we prevent user input in the terminal from mixing with the alerts or getting overridden by new data?
+
+        
+        
+        menu_helper = menu.Menu()
+        
+        
+        next_menu = menu_helper.get_user_input(">> ",menu_helper.choice_set)
+        
         menu_helper.navigate_next_menu(next_menu)
