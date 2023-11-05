cd4c3c74376ffa1f4d9b5c0669cb0975a2189221
SandraBarba <43258459+SandraBarba@users.noreply.github.com>
2023-11-05 15:09:37 -0700

    update

--- CLI/Alerts.py
+++
@@ -1,9 +0,0 @@
-#alerts.py
-
-class Alerts:
-    def __init__(self, level, time, IP, Port, description):
-        self.level = level
-        self.time = time
-        self.IP = IP
-        self.Port = Port
-        self.description = description
