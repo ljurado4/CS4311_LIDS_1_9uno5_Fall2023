759ced45d59d339a1e7eb9e267c75e8cb45389e0
Benjamin H <benjaminhansen836@gmail.com>
2023-11-12 16:43:56 -0700

    Update LIDS-D alerts Table

--- LIDS_D/static/js/alerts_table.js
+++ LIDS_D/static/js/alerts_table.js
@@ -21,38 +21,54 @@ function updateTable(data) {
     // Get the table body element
     const tableBody = document.querySelector('#TableID tbody');
     
+    console.log("Before foor loop"+data)
     // Clear the table body
     tableBody.innerHTML = '';
     
     // Loop through the alert data and create table rows
     data.forEach((alert, index) => {
+       
+
         const row = tableBody.insertRow(index);
         row.id = `alert${index + 1}`;
         
         // Create table cells for alert properties
-        const levelCell = row.insertCell(0);
-        levelCell.id = `alert${index + 1}-level`;
-        levelCell.textContent = alert.Level; 
-        
-        const timeCell = row.insertCell(1);
+        const timeCell = row.insertCell(0);
         timeCell.id = `alert${index + 1}-time`;
         timeCell.textContent = alert.Time; 
-        
-        const ipCell = row.insertCell(2);
-        ipCell.id = `alert${index + 1}-ip`;
-        ipCell.textContent = alert.Source; 
-        
-        const portCell = row.insertCell(3);
-        portCell.id = `alert${index + 1}-port`;
-        portCell.textContent = alert.Port; 
-
-        const identifierCell = row.insertCell(4);
+    
+        const identifierCell = row.insertCell(1);
         identifierCell.id = `alert${index + 1}-identifier`;
-        identifierCell.textContent = alert.Port; 
-
-        const descriptionCell = row.insertCell(5);
+        identifierCell.textContent = alert.Identifier; 
+    
+        const levelCell = row.insertCell(2);
+        levelCell.id = `alert${index + 1}-level`;
+        levelCell.textContent = alert.Level; 
+    
+        const sourceIpCell = row.insertCell(3);
+        sourceIpCell.id = `alert${index + 1}-sourceIp`;
+        sourceIpCell.textContent = alert.SourceIP; 
+    
+        const sourcePortCell = row.insertCell(4);
+        sourcePortCell.id = `alert${index + 1}-sourcePort`;
+        sourcePortCell.textContent = alert.SourcePort;
+    
+        const destIpCell = row.insertCell(5);
+        destIpCell.id = `alert${index + 1}-destIp`;
+        destIpCell.textContent = alert.DestIP; 
+    
+        const destPortCell = row.insertCell(6);
+        destPortCell.id = `alert${index + 1}-destPort`;
+        destPortCell.textContent = alert.DestPort;
+    
+        const typeAlertCell = row.insertCell(7);
+        typeAlertCell.id = `alert${index + 1}-typeAlert`;
+        typeAlertCell.textContent = alert.TypeAlert; 
+    
+        const descriptionCell = row.insertCell(8);
+        descriptionCell.id = `alert${index + 1}-description`;
         descriptionCell.innerHTML = `<button id="alert${index + 1}-button" class="alertDescriptionButton" onclick="displayAlert(this.id)">${alert.Description}</button>`; 
-    });
+     });
 }
 
 // Socket.IO event listener for the 'new_alert_data' event
