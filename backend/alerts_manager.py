0c690f21e0f7c1b2ea683b15f5885f9bbb8b706c
Lizbeth <ljurado4@miners.utep.edu>
2023-12-02 18:28:58 -0700

    Revert "LNIDS Connections, Headless Alerts collected"

--- backend/alerts_manager.py
+++ backend/alerts_manager.py
@@ -1,12 +1,9 @@
 # File: alerts_manager.py
 #
-# Description: This class is designed to manage and create alerts within a system 
-# and has some logging functionality. Now modified to facilitate headless alerts 
-# output to the CLI.
+# Description: This class is designed to manage and create alerts within a system and has some logging functionality.
 # 
 # @ Author: Alejandro Hernandez
 # @ Modifier: Alejandro Hernandez
-# @ Modifier: Lizbeth Jurado 
 
 import logging
 from backend import Alerts
@@ -17,20 +14,14 @@ class AlertManager:
 
     def __init__(self):
         self.alerts = []
-        self.setup_logging()
+# @ Author: Alejandro Hernandez
 
-    # @ Modified: Lizbeth Jurado 
     def setup_logging(self):
-        # Set up logging to a file with an enhanced format for better readability
-        logging.basicConfig(
-            filename='alerts.log',
-            level=logging.INFO,
-            format='%(asctime)s:%(levelname)s:%(message)s'
-        )
+        # Set up logging to a file
+        logging.basicConfig(filename='alerts.log', level=logging.INFO)
 
-    # @ Modified: Lizbeth Jurado 
     def create_alert(self, pcap_data, time, identifier, level, sourceIP, sourcePort, destIP, destPort, typeAlert, description):
-        # Create an alert instance with the provided details
+
         alert = Alerts.Alerts(time, identifier, level, sourceIP, sourcePort, destIP, destPort, typeAlert, description)
         alert.PCAPData = pcap_data  # Set the PCAPData property
         self.alerts.append(alert)
@@ -38,21 +29,13 @@ class AlertManager:
         
         # Log the alert
         logging.info(f"Alert created: {description} at {time}")
-
-        # Handle alert for CLI (headless alert)
-        self.handle_cli_alert(alert)
-
-    # @ Added: Lizbeth Jurado 
-    def handle_cli_alert(self, alert):
-        # Output alert details to console for CLI
-        print(f"Headless Alert Generated:\nTime: {alert.time}\nIdentifier: {alert.identifier}\nLevel: {alert.level}\nSource IP: {alert.sourceIP}\nSource Port: {alert.sourcePort}\nDestination IP: {alert.destIP}\nDestination Port: {alert.destPort}\nType: {alert.typeAlert}\nDescription: {alert.description}\n")
-
-    # @ Author: Alejandro Hernandez
+    
+# @ Author: Alejandro Hernandez
+    
     def ident_list(self, packet, identifier):
-        # Keep a list of identifiers for each packet
         self.identifierList.append({identifier: [packet]})
 
-    # @ Author: Alejandro Hernandez
+# @ Author: Alejandro Hernandez
+    
     def get_alerts(self):
-        # Retrieve the list of generated alerts
         return self.alerts
--- backend/packet_analyzer.py
+++ backend/packet_analyzer.py
@@ -1,77 +1,80 @@
 # File: packet_analyzer.py
 #
-# Description: Adds functionality to analyze network packets and manage alerts, now including packet storage capabilities.
+# Description:
 #
 # @ Author: Alejandro Hernandez
 # @ Modifier: Lizbeth Jurado
 
+
 from . import ipChecker, alerts_manager, loginCheck, PortChecker
 from datetime import datetime
 import pyshark
-import json
 
 class PacketAnalyzer:
     def __init__(self):
         self.loginCheck = loginCheck.LoginCheck()
+        self.packetAnalyzer = None
         self.iC = ipChecker.ip_Checker()
         self.getAlerts = alerts_manager.AlertManager()
         self.portCheck = PortChecker.portDetection()
 
-    # @ Modified for Packet Storage: Lizbeth Jurado
-    def analyze_packet(self, packet, time, identifier, sourceIP, sourcePort, destIP, destPort, protocol, handshake, packetList):
-        if self.login_attempts(packet, protocol, destPort, time) == True:
-            self.create_alert(packet, time, identifier, 3, sourceIP, sourcePort, destIP, destPort, "Failed Login Error", "Multiple failed logins detected")
-            self.store_packet_data(packet, identifier)
+# @ Author: Alejandro Hernandez
+    def analyze_packet(self, packet, time, identifier, sourceIP, sourcePort,destIP,destPort,protocol,handshake,packetList):
 
-        if not self.ip_check(sourceIP):
+        if self.login_attempts(packet,protocol,destPort,time) == True:
+            self.create_alert(packet, time, identifier, 3, sourceIP, sourcePort,destIP,destPort,"Failed Login Error","Multiple failed logins detected")
+
+        if self.ip_check(sourceIP) == False:
+
             res = self.port_scan_check(sourceIP, destPort, time, handshake, packetList)
             if res == "threshold1":
-                self.create_alert(packet, time, identifier, 2, sourceIP, sourcePort, destIP, destPort, "Port Scan Error", "Port Scan surpassing threshold1")
-                self.store_packet_data(packet, identifier)
-        elif not self.ip_check(sourceIP):
-            self.create_alert(packet, time, identifier, 1, sourceIP, sourcePort, destIP, destPort, "Unknown IP Error", "Source IP detected that is not part of approved IP list")
-            self.store_packet_data(packet, identifier)
+                self.create_alert(packet, time, identifier, 2, sourceIP, sourcePort,destIP,destPort,"Port Scan Error","Port Scan surpassing threshold1")
+        elif self.ip_check(sourceIP) == False:
+            self.create_alert(packet, time, identifier, 1, sourceIP, sourcePort,destIP,destPort,"Unknown IP Error","Source IP detected that is not appart of approved IP list")
 
-    # @ Author: Alejandro Hernandez
+# @ Author: Alejandro Hernandez
+    
     def ip_check(self, IP):
+        # print("IP check ",IP)
         return self.iC.ip_in_List(IP)
 
-    # @ Author: Alejandro Hernandez
+# @ Author: Alejandro Hernandez
     def port_scan_check(self, IP, destPort, time, handshake, packetList):
         threshold1 = 300
         timeAllowed = 700
-        timeOF = datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
-        if handshake:
+        timeOF = datetime.strptime(time,"%Y-%m-%d %H:%M:%S.%f")
+        # print("Reach")
+        # print("port_scan_check")
+        if handshake == True:
+            # print("handshake")
             return self.portCheck.port_Checking(IP, destPort, timeOF, timeAllowed, threshold1, packetList)
     
-    # @ Author: Alejandro Hernandez
-    def login_attempts(self, packet, protocol, destPort, time):
+# @ Author: Alejandro Hernandez
+    
+    def login_attempts(self, packet,protocol,destPort,time):
         threshold = 700
-        timeOF = datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
-        return self.loginCheck.failedPssWrd(packet, protocol, timeOF, destPort, threshold)
+        timeOF = datetime.strptime(time,"%Y-%m-%d %H:%M:%S.%f")
+        return self.loginCheck.failedPssWrd(packet,protocol,timeOF,destPort,threshold)
 
-    # @ Author: Alejandro Hernandez
-    def create_alert(self, packet, time, identifier, level, sourceIP, sourcePort, destIP, destPort, typeAlert, description):
+# @ Author: Alejandro Hernandez
+    
+    def create_alert(self, packet, time, identifier, level, sourceIP, sourcePort,destIP,destPort,typeAlert,description):
+        # Capture PCAP data as a string from the packet
         pcap_data = str(packet)
-        self.getAlerts.create_alert(pcap_data, time, identifier, level, sourceIP, sourcePort, destIP, destPort, typeAlert, description)
+        # Call the AlertsManager class to create an alert
+        self.getAlerts.create_alert(pcap_data,time, identifier, level, sourceIP, sourcePort,destIP,destPort,typeAlert,description)
         self.getAlerts.ident_list(pcap_data, identifier)
+        alerts = self.getAlerts.get_alerts()
 
-    # @ Modifier for Packet Storage: Lizbeth Jurado
-    def store_packet_data(self, packet_data, identifier):
-        # Serialize packet data and save it to a JSON file
-        try:
-            with open(f'packet_data_{identifier}.json', 'w') as outfile:
-                json.dump(packet_data, outfile, indent=4)
-        except Exception as e:
-            print(f"An error occurred while storing the packet data: {e}")
-
-    # @ Author: Lizbeth Jurado
+# @ Author: Lizbeth Jurado
     def read_pcap(self, pcap_file_path):
         try:
+            # Open the pcap file with FileCapture
             cap = pyshark.FileCapture(pcap_file_path, only_summaries=True)
             packets_data = []
 
             for packet in cap:
+                # Process each packet into a dictionary
                 packet_dict = {
                     'number': packet.no,
                     'time': packet.time,
@@ -85,10 +88,12 @@ class PacketAnalyzer:
 
             cap.close()
             return packets_data
+
         except Exception as e:
             print(f"An error occurred while reading the pcap file: {e}")
             return []
 
-    # @ Author: Lizbeth Jurado
+    # format_for_frontend method
     def format_for_frontend(self, packet_data):
-        return json.dumps(packet_data, indent=4)
+        # Convert packet data to JSON
+        return json.dumps(packet_data)
