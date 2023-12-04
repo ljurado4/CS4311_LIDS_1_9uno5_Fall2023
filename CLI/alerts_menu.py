5ec3fc42d7f91edc31f7f5b55b7773850a36a36b
Lizbeth <ljurado4@miners.utep.edu>
2023-12-03 23:43:49 -0700

    Update alerts_menu.py

--- CLI/alerts_menu.py
+++ CLI/alerts_menu.py
@@ -3,7 +3,9 @@
 # Description: Displaying alerts through a command-line interface (CLI). The class includes a method called display_Alerts that initiates the capture of network packets and then prints a list of alerts obtained from an AlertManager
 
 # @ Author: Benjamin Hansen
-# @ Modifier: Alejandro Hernandez, Thomas Sandoval, Sandra Barba
+# @ Modifier: Alejandro Hernandez
+# @ Modifier: Thomas Sandoval
+# @ Modifier: Sandra Barba
 
 
 import sys
